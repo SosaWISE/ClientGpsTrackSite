58c1b1569e2cf3e6f91557df33bd39b0d531ca80
Andres Sosa <SosaWISE@gmail.com>
2012-10-25 15:53:28 -0600

    Adding Polygon Fences to the maps.

--- js/gps/mapsV3.js
+++ js/gps/mapsV3.js
@@ -11,6 +11,9 @@ Ext.ns("SOS.Gps");
 SOS.Gps.Maps = (function ()
 {
 	/** START MEMBER Variables */
+	var _geocoder;
+	var _currentMap;
+	var _fenceHashTable = {};
 	/**   END MEMBER Variables */
 
 	/** START MEMBER Functions */
@@ -20,12 +23,77 @@ SOS.Gps.Maps = (function ()
 			/** Initialize */
 			console.log(oOptions);
 			//debugger;
-			var map = new google.maps.Map(document.getElementById("map_canvas"),{zoom: 14,
+			_currentMap = new google.maps.Map(document.getElementById("map_canvas"),{zoom: 14,
 				center: new google.maps.LatLng(oOptions.latitude, oOptions.longitude),
 				mapTypeId: google.maps.MapTypeId.ROADMAP
 			});
 
+			/** Set the Geocoder. */
+			_geocoder = new google.maps.Geocoder();
 		}
+
+		, CenterMapFromAddress: function (street, city, state, zip)
+		{
+			/** Initialize. */
+			var address = $.validator.format("{0}; {1} {2} {3}"
+				, street
+				, city
+				, state
+				, zip);
+			_geocoder.geocode(
+				{ 'address': address}
+				, function (results, status)
+				{
+					if (status === google.maps.GeocoderStatus.OK)
+					{
+						_currentMap.setCenter(results[0].geometry.location);
+						var marker = new google.maps.Marker({
+							map: _currentMap,
+							position: results[0].geometry.location
+						});
+					}
+					else
+					{
+						alert("Geocoder was not successful for the following reason: \r" + status);
+					}
+				}
+			);
+		}
+
+		/***
+		 * This sets the fences on the map.
+		 */
+		, PaintGeoFences: function (fencesArray)
+		{
+			/** Initialize. */
+			_fenceHashTable = {};
+			$.each(fencesArray, function (index, item)
+			{
+				debugger;
+				switch (item.GeoFenceTypeId)
+				{
+					case "PNT":
+						break;
+					case "CIR":
+						break;
+					case "POLY":
+						_fenceHashTable[item.GeoFenceID] = new google.maps.Polygon({
+							map: _currentMap
+							, strokeColor: '#ff0000'
+							, strokeOpacity: 0.6
+							, strokeWeight: 4
+							, path: []
+						});
+						break;
+				}
+			});
+		}
+		/** START MEMBER Variables. */
+		, get FenceTable()
+		{
+			return _fenceHashTable;
+		}
+		/**   END MEMBER Variables. */
 	};
 	/**   END MEMBER Functions */
 })();
